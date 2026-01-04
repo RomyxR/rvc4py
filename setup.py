7a263a89487a5c2a00b9d97e8b4be68195ce42da
Romyx <157620445+RomyxR@users.noreply.github.com>
2026-01-04 18:05:33 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="rvc4py",
+    version="0.0.1",
+    description="Simple RVC inference in Python",
+    python_requires=">=3.10",
+    license="MIT",
+    install_requires=[
+        "numpy<2.0",
+        "soundfile==0.12.1",
+        "librosa==0.10.1",
+        "praat-parselmouth==0.4.3",
+        "pyworld==0.3.4",
+        "torchcrepe==0.0.22",
+        "av>=10.0.0",
+        "faiss-cpu==1.7.4",
+        "python-dotenv==1.0.0",
+        "pydub==0.25.1",
+        "click==8.1.7",
+        "tensorboardx==2.6.2.2",
+        "poethepoet==0.24.4",
+        "numba==0.58.1",
+        "tqdm",
+        "torch",
+        "torchaudio",
+        "fairseq @ git+https://github.com/Tps-F/fairseq.git",
+    ],
+)
